b01b046215dabccba2386ea772e6d182bb575f11
Lukas Georgieff <lukas.georgieff@hotmail.com>
2018-04-13 23:28:50 -0700

    src: Add basic mongodb functionality to client

--- backend/src/http/main.cpp
+++ backend/src/http/main.cpp
@@ -3,12 +3,19 @@
 #include <proxygen/httpserver/RequestHandlerFactory.h>
 #include <proxygen/httpserver/ResponseBuilder.h>
 
+#include <bsoncxx/builder/stream/document.hpp>
+#include <bsoncxx/json.hpp>
+#include <mongocxx/client.hpp>
+#include <mongocxx/instance.hpp>
+
 #include <cstddef>
 #include <chrono>
 #include <vector>
 #include <set>
 #include <string>
 #include <iostream>
+
+mongocxx::instance inst{};
 
 static const std::string HTTP_IP_ADDRESS("0.0.0.0");
 static const uint16_t HTTP_PORT(80);
@@ -30,30 +37,43 @@ static const std::set<std::string> HTTP_CONTENT_COMPRESSION_TYPES{
     "text/plain",
     "text/xml"};
 
-class HelloWorldStats {
- public:
-   virtual ~HelloWorldStats() {}
-
-   virtual void recordRequest() { ++reqCount_; }
-
-   virtual uint64_t requestCount() const { return reqCount_; }
-
- private:
-  uint64_t reqCount_{0};
-};
-
 class HelloWorldHandler : public proxygen::RequestHandler {
 public:
-  explicit HelloWorldHandler(HelloWorldStats *stats) : stats_{stats} {}
+  explicit HelloWorldHandler() {}
 
   void onRequest(std::unique_ptr<proxygen::HTTPMessage> headers) noexcept {}
 
   void onBody(std::unique_ptr<folly::IOBuf> body) noexcept {}
 
   void onEOM() noexcept {
+    mongocxx::client conn{mongocxx::uri{"mongodb://low_mongodb:27017"}};
+
+    using stream_document = bsoncxx::builder::stream::document;
+    using document_value = bsoncxx::stdx::optional<bsoncxx::document::value>;
+    using bsoncxx::builder::stream::close_document;
+    using bsoncxx::builder::stream::finalize;
+    using bsoncxx::builder::stream::open_document;
+
+    stream_document document{};
+    auto collection = conn["testdb"]["testcollection"];
+
+    document_value maybe_result = collection.find_one({});
+    int64_t counter{1};
+    if (maybe_result) {
+      counter = maybe_result->view()["counter"].get_int64();
+      document << "counter" << counter;
+      collection.update_one(
+          stream_document{} << "counter" << counter << finalize,
+          stream_document{} << "$set" << open_document << "counter" << ++counter
+                            << close_document << finalize);
+    } else {
+      document << "counter" << counter;
+      collection.insert_one(document.view());
+    }
+
     proxygen::ResponseBuilder(downstream_)
         .status(200, "OK")
-        .body("Hello World! #" + std::to_string(stats_->requestCount()))
+        .body("Hello World! #" + std::to_string(counter))
         .sendWithEOM();
   }
 
@@ -64,24 +84,17 @@ public:
   }
 
   void onError(proxygen::ProxygenError err) noexcept { delete this; }
-
-private:
-  HelloWorldStats *stats_;
 };
 
 class HelloWorldHandlerFactory : public proxygen::RequestHandlerFactory {
 public:
-  void onServerStart(folly::EventBase *) noexcept override { stats_.reset(new HelloWorldStats); }
+  void onServerStart(folly::EventBase *) noexcept override {}
 
-  void onServerStop() noexcept override { stats_.reset(); }
+  void onServerStop() noexcept override {}
 
   proxygen::RequestHandler* onRequest(proxygen::RequestHandler*, proxygen::HTTPMessage*) noexcept override {
-    stats_->recordRequest();
-    return new HelloWorldHandler(stats_.get());
+    return new HelloWorldHandler();
   }
-
-private:
-  folly::ThreadLocalPtr<HelloWorldStats> stats_;
 };
 
 int main(const int argc, const char **argv) {
